31ccae9e9051484deef4fa42e5bf510f7adc0dda
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-11-18 01:13:09 +0900

    Complete 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+  for (arg of args){
+    while (arr.indexOf(arg) !== -1)
+      arr.splice(arr.indexOf(arg), 1);
+  }
+  return arr;
 };
 
 // Do not edit below this line
