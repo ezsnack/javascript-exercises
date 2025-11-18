98b2d5647130631b9f8c9f7992517ea45559d544
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-11-18 13:23:59 +0900

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(intOne, intTwo) {
+  if (intOne <= 0 || intTwo <= 0 ||
+      parseInt(intOne) !== intOne || parseInt(intTwo) !== intTwo ||
+      isNaN(intOne) || isNaN(intTwo))
+        return "ERROR";
+      
+  if (intOne == intTwo) return intOne;
+  let lower, higher;
+  let sum = 0;
+  if (intOne < intTwo) {
+    lower = intOne;
+    higher = intTwo;
+  } else {
+    lower = intTwo;
+    higher = intOne;
+  }
+  for (let i = lower; i <= higher; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
