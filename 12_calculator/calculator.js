093fcbe40762714c3035094e765848d8373e3e66
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-12-06 23:49:28 +0900

    Complete 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, number) => sum * number, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x == 0) return 1;
+  if (x < 0) return undefined;
+  let result = 1;
+	for (let i=2; i <= x; ++i)
+    result *= i;
+  return result;
 };
 
 // Do not edit below this line
