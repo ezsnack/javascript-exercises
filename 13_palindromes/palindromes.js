52d78f70365bafa53187bcc38855b7fbfd6a472c
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-12-07 00:22:13 +0900

    Complete 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let lettersOnly = "";
+  const lowercaseString = string.toLowerCase();
+  for (let i = 0; i < lowercaseString.length; ++i) {
+    const charCode = lowercaseString.charCodeAt(i);
+    if ((charCode > 96 && charCode < 123) || (charCode > 47 && charCode < 58)) //apparently the test tests for numbers as well, which was not mentioned in the readme
+      lettersOnly += lowercaseString[i];
+  }
+  const reversedString = lettersOnly.split("").reverse().join("");
+  if (reversedString === lettersOnly)
+    return true;
+  else
+    return false;
 };
 
 // Do not edit below this line
