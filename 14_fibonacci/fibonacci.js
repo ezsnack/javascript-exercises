b16498330aaed118f9f465927749a1c6e7eb97b9
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-12-07 23:19:31 +0900

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let x = 0;
+  let y = 1;
+  if (num == 0)
+    return x;
+  if (num < 0)
+    return "OOPS";
+  for (let i = 1; i < num; i++) {
+    const temp = y;
+    y += x;
+    x = temp;
+  }
+  return y;
 };
 
 // Do not edit below this line
