cdcf2c63454df470c901907004f968630a6a6bc1
ezsnack <204544689+ezsnack@users.noreply.github.com>
2025-12-11 14:14:30 +0900

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldest = {};
+  let highestAge = 0;
+  const currentYear = parseInt(new Date().getFullYear());
+  for (let person of people) {
+    const year = person.yearOfDeath ? person.yearOfDeath : currentYear;
+    const age = year - person.yearOfBirth;
+    if (age > highestAge) {
+      highestAge = age;
+      oldest = person;
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
